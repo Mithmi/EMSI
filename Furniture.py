e3c6af35a33d4d73ed37ac1477bd76e91af0a130
Mithmi <workaintfun@yahoo.com>
2017-06-26 16:44:10 +0400

    0.02
    Added formulas

--- Furniture.py
+++ Furniture.py
@@ -2,7 +2,7 @@
 class Furniture:
     def __init__(self):
         self.name
-        self.elemets = []
+        self.elements = []
         self.price
 
     def total_price(self):
@@ -10,18 +10,29 @@ class Furniture:
             self.prioe += self.elemets[i].price
         return self.price
 
+"""
+Calculation of total cost for Wood - Volume * Price of 1 cube of material + 0.25*(Price of paint * Number of Layers) * Square of Surface
+Calculation of total cost for MDF - Square * Price of 1 square meter of material + 0.25*(Price of paint * Number of Layers) * Square of Surface
+Calculation of details per template - RoundUp(TemplateWidth - DetailWidth) * RoundUp(TemplateLength - DetailLength)
+Calculation of loss coefficient for MDF - MAX(TemplateSquare - DetailSquare)
+Calculation of Additive price - ClearPrice * Loss coefficient
+Calculation of Material price - ClearPrice + AdditivePrice/TemplateSquare
+"""
+
+
 class FurnitureElement:
-    def __init__(self, name, width, length, depth, price, paint):
+    def __init__(self, name, width, length, depth, price, paint, material):
         self.name = name
         self.width = width
-        self.lenght = length
+        self.length = length
         self.depth = depth
         self.price = price
         self.paint = paint
+        self.material = material
 
     def show(self):
-        print(self.name, str(self.width) + "x" + str(self.lenght) + "x" + str(self.depth), self.price, self.paint)
+        print(self.name, str(self.width) + "x" + str(self.length) + "x" + str(self.depth), self.price, self.paint, self.material)
 
 
-test = FurnitureElement("Box", 600, 300, 400, 15000, "White")
+test = FurnitureElement("Box", 600, 300, 400, 15000, "White", "Oak")
 test.show()
